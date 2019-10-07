001db5656c8252f907973524eadb001a6a5bc7af
Eason Lin <uu9924079@gmail.com>
2019-10-07 17:31:28 +0800

    add sort feature

--- routes/home.js
+++ routes/home.js
@@ -4,18 +4,44 @@ const router = express.Router()
 const db = require('../models')
 const Record = db.Record
 const User = db.User
+const Sequelize = require('sequelize')
+const Op = Sequelize.Op
 // const sumAmount = require('../public/javascripts/sumAmount')
 const { authenticated } = require('../config/auth')
 
 // 首頁
 router.get('/', authenticated, (req, res) => {
+  let selectedCategory = null
+  let selectedMonth = null
+  if (req.query.category) selectedCategory = req.query.category
+  if (req.query.month) selectedMonth = `${req.query.month}`
+  console.log(selectedCategory, selectedMonth)
   User.findByPk(req.user.id)
     .then((user) => {
       if (!user) throw new Error("user not found")
       // get current user's todo list
-      return Record.findAll()
+      if (!selectedCategory && !selectedMonth) {
+        return Record.findAll({
+          where: { UserId: req.user.id }
+        })
+      }
+      if (selectedCategory && selectedMonth) {
+        return Record.findAll({
+          where: { UserId: req.user.id, category: selectedCategory, date: { [Op.like]: `_____${selectedMonth}___` } }
+        })
+      }
+      if (selectedCategory) {
+        return Record.findAll({
+          where: { UserId: req.user.id, category: selectedCategory }
+        })
+      }
+      if (selectedMonth) {
+        return Record.findAll({
+          where: { UserId: req.user.id, date: { [Op.like]: `_____${selectedMonth}___` } }
+        })
+      }
     })
-    .then((records) => { return res.render('index', { records: records }) })
+    .then((records) => { return res.render('index', { records, queryMonth: req.query.month, queryCategory: req.query.category }) })
     .catch((error) => { return res.status(422).json(error) })
 })
 
