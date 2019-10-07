e30988960b4f9cd2c7684555e4b4390502d95756
Eason Lin <uu9924079@gmail.com>
2019-10-07 14:38:16 +0800

    add users route

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use((req, res, next) => {
 /* -----route setting----- */
 app.use('/', require('./routes/home'))
 // app.use('/records', require('./routes/record'))
-// app.use('/users', require('./routes/user'))
+app.use('/users', require('./routes/user'))
 // app.use('/auth', require('./routes/auths'))
 
 app.listen(process.env.PORT || 3000, () => {
